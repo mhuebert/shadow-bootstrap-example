982bb96e26508743b7aaaf47f81bdd2bb516b699
Matthew Huebert <no-email@gmail.com>
2017-10-05 21:34:21 +0200

    Clean up

--- src/shadow_eval/core.cljs
+++ src/shadow_eval/core.cljs
@@ -22,25 +22,17 @@
 (defn render []
   (v/render-to-dom (layout) "shadow-eval"))
 
-(defn print-result [{:keys [error value] :as result}]
-  (js/console.log "result" result)
-  (js/console.log "compile-state" @boot/compile-state-ref)
-  (set! (.-innerHTML (js/document.getElementById "dump")) value))
-
-(defn compile-it []
-  (cljs/eval-str
-    boot/compile-state-ref
-    "(ns my.user (:require [reagent.core :as r]))
-     (doall (for [n (range 10)] n))
-     (map inc [1 2 3])"
-    "[test]"
-    {:eval
-           (fn [{:keys [source cache lang name]}]
-             (js/console.log "Eval" name lang {:cache  (some-> cache (str) (subs 0 20))
-                                               :source (some-> source (subs 0 150))})
-             (js/eval source))
-     :load boot/load}
-    print-result))
-
-(defn start []
-  (boot/init compile-it))
+(comment (defn compile-it []
+           (cljs/eval-str
+             boot/compile-state-ref
+             "(ns my.user (:require [re-view.core :as v :refer [defview]]))
+              (doall (for [n (range 10)] n))
+              (map inc [1 2 3])"
+             "[test]"
+             {:eval
+                    (fn [{:keys [source cache lang name]}]
+                      (js/console.log "Eval" name lang {:cache  (some-> cache (str) (subs 0 20))
+                                                        :source (some-> source (subs 0 150))})
+                      (js/eval source))
+              :load boot/load}
+             print-result)))
